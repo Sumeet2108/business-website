e05c563235c0ca7ba9362b6b6ba1bf0f7ba3d29a
yash-sanaye <yash.sanaye@live.com>
2024-12-15 13:19:26 +0530

    contactus import

---
+++ app/contactus/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Contactform from "@/components/ui/contactform";
+const page = () => {
+  return (
+    <div>
+      page
+      <div>
+        <Contactform />
+      </div>
+    </div>
+  );
+};
+
+export default page;
